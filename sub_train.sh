a91e62b81356820d525a65129f58386073624c96
inferno-cat <bili_a1@outlook.com>
2025-05-02 00:21:20 +0800

    raw

--- sub_train.sh
+++ sub_train.sh
@@ -1,4 +1,4 @@
-python sub_main6_CoordAttn.py \
+python sub_main7_raw_shu.py \
 --dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
 --train_batch_size 20 \
 --sampler_num 20000 \
