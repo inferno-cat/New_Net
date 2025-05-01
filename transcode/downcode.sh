8c2b4e55e5cb7c3cbb631b90e60ad2db84bb703b
inferno-cat <bili_a1@outlook.com>
2025-05-01 19:33:48 +0800

    hello

--- transcode/downcode.sh
+++ transcode/downcode.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# 该脚本用于一键克隆 GitHub 项目到服务器本地
-# 设置 GitHub 仓库地址，请根据实际情况修改
-git_repo="git@github.com:inferno-cat/New_Net.git"
-# 设置克隆的目标目录，请根据实际情况修改
-clone_dir="/home/share3/zc/file/New_Net"
-# 克隆仓库
-git clone $git_repo $clone_dir
-echo "项目克隆完成！"
+# 该脚本用于一键获取 GitHub 项目更新到服务器本地
+# 设置项目目录路径，请根据实际情况修改
+project_dir="/home/share3/zc/file/New_Net"
+# 切换到项目目录
+cd $project_dir
+# 拉取最新更新
+git pull origin main
+echo "项目更新完成！"
