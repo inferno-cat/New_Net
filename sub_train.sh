c090910facea80a497eca46cf0de140ee9c9bf0c
inferno-cat <bili_a1@outlook.com>
2025-05-02 00:28:11 +0800

    12

--- sub_train.sh
+++ sub_train.sh
@@ -2,4 +2,6 @@ python sub_main7_raw_shu.py \
 --dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
 --train_batch_size 20 \
 --sampler_num 20000 \
---store_folder ./output/CoordAttn
+--store_folder ./output/CoordAttn \
+--epochs 30 \
+--num_workers 12
