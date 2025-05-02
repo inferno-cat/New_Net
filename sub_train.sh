2fffce5e1b9ef1cc6eaacb0ed5eb0707d8a42695
inferno-cat <bili_a1@outlook.com>
2025-05-02 22:24:20 +0800

    up6

--- sub_train.sh
+++ sub_train.sh
@@ -3,13 +3,13 @@ python sub_main_base.py \
 --train_batch_size 8 \
 --sampler_num 20 \
 --store_folder ./output/base \
---epochs 3
+--epochs 10
 python sub_main_base.py \
 --dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
 --train_batch_size 8 \
 --sampler_num 20 \
 --store_folder ./output/base \
 --epochs 25 \
---resume ./output/base/checkpoints/epoch-3-ckpt.pt
+--resume ./output/base/checkpoints/epoch-10-ckpt.pt
 
 
