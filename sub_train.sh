a235c19627f7a27b5ff9e380165e54d8568b6ef1
inferno-cat <bili_a1@outlook.com>
2025-05-05 09:03:13 +0800

    002_4

--- sub_train.sh
+++ sub_train.sh
@@ -79,14 +79,14 @@
 #--epochs 25 \
 #--num_workers 8
 
-python sub_main001.py \
---dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
---train_batch_size 32 \
---sampler_num 20000 \
---store_folder ./output/001 \
---epochs 35 \
---num_workers 8 \
---resume /home/share3/zc/file/New_Net/output/001/checkpoints/epoch-13-ckpt.pt
+#python sub_main001.py \
+#--dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
+#--train_batch_size 32 \
+#--sampler_num 20000 \
+#--store_folder ./output/001 \
+#--epochs 35 \
+#--num_workers 8 \
+#--resume /home/share3/zc/file/New_Net/output/001/checkpoints/epoch-13-ckpt.pt
 
 python sub_main002.py \
 --dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
@@ -94,4 +94,4 @@ python sub_main002.py \
 --sampler_num 20000 \
 --store_folder ./output/002 \
 --epochs 25 \
---num_workers 12
+--num_workers 12 
