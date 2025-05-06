d0e2d0d7149665a9bf7be47cb67b67e1a1e25f5f
inferno-cat <bili_a1@outlook.com>
2025-05-06 22:53:38 +0800

    pddp

--- sub_train.sh
+++ sub_train.sh
@@ -113,20 +113,29 @@
 #--num_workers 12 \
 #--loss_method AW
 
-python sub_main001.py \
+#python sub_main001.py \
+#--dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
+#--train_batch_size 64 \
+#--sampler_num 30000 \
+#--store_folder ./output/001_crop320_MSPA_up_before_res \
+#--epochs 35 \
+#--num_workers 12 \
+#--loss_method HFL
+#
+#python sub_main001.py \
+#--dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
+#--train_batch_size 64 \
+#--sampler_num 30000 \
+#--store_folder ./output/001_crop320_MSPA_up_before_res_loss \
+#--epochs 35 \
+#--num_workers 12 \
+#--loss_method AW
+
+python sub_main_mix01.py \
 --dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
 --train_batch_size 64 \
 --sampler_num 30000 \
---store_folder ./output/001_crop320_MSPA_up_before_res \
+--store_folder ./output/PDDP_crop320 \
 --epochs 35 \
 --num_workers 12 \
 --loss_method HFL
-
-python sub_main001.py \
---dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
---train_batch_size 64 \
---sampler_num 30000 \
---store_folder ./output/001_crop320_MSPA_up_before_res_loss \
---epochs 35 \
---num_workers 12 \
---loss_method AW
