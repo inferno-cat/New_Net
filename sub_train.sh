7a554c95f0d2aa07b406d3d86042499a3d4e9579
inferno-cat <bili_a1@outlook.com>
2025-05-09 23:22:53 +0800

    05

--- sub_train.sh
+++ sub_train.sh
@@ -285,7 +285,7 @@ python new_main_05.py \
 --loss_method HFL \
 --print_freq 500 \
 --lr_stepsize 5 \
---learning_rate 1e-4
+--learning_rate 1e-4 
 
 #python new_main_06.py \
 #--dataset /home/share/liuchangsong/edge_data/BSDS500_flip_rotate_pad/ \
